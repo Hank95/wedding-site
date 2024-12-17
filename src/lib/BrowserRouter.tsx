9346cfd2b0aef02384edb1ff91eda7efc34ea59b
Henry Pendleton <hhpendleton@gmail.com>
2024-12-17 14:24:17 -0500

    Add GalleryPage component and update routing

--- src/lib/BrowserRouter.tsx
+++ src/lib/BrowserRouter.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import ErrorPage from "../pages/ErrorPage";
 import ContactPage from "../pages/ContactPage";
 import RegistryPage from "../pages/RegistryPage";
+import GalleryPage from "../pages/GalleryPage";
 
 const BrowserRouter = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const BrowserRouter = createBrowserRouter([
       {
         path: "registry",
         element: <RegistryPage />,
+      },
+      {
+        path: "gallery",
+        element: <GalleryPage />,
       },
     ],
   },
